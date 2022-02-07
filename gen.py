bde6eb5c7fe7bee9ff64e21d26047d51387cc864
Mathias Brossard <mathias@brossard.org>
2022-02-06 23:50:06 -0600

    Add LPCXpresso54114 target

--- gen.py
+++ gen.py
@@ -7,6 +7,7 @@ targets = {
     "FRDM-K82F":                 "K82F",
     "LPCXpresso4337":            "LPC4337_EVK",
     "LPCXpresso4367":            "LPC4330_M4",
+    "LPCXpresso54114":           "LPC54114",
     "LPCXpresso54608":           "LPC546XX",
     "LPCXpresso55S69":           "LPC55S69_NS",
     "MAX32620":                  "MAX32620FTHR",
